bb50bc962591bef81ac22665aa7f82218941e420
inouema <masatoinoue00@gmail.com>
2012-03-24 14:42:36 +0900

    add these files.

---
+++ project/rtl/altip/alt_vram_attribute_inst.v
@@ -0,0 +1,8 @@
+alt_vram_attribute	alt_vram_attribute_inst (
+	.clock ( clock_sig ),
+	.data ( data_sig ),
+	.rdaddress ( rdaddress_sig ),
+	.wraddress ( wraddress_sig ),
+	.wren ( wren_sig ),
+	.q ( q_sig )
+	);
